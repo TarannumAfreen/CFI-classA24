e7c5f084099e2dca382bcafa6322f21de891fdc9
Tarannum Afreen <tarannnum11@gmail.com>
2024-06-01 12:21:08 +0530

    leet code problem

--- Phase-11/1.cjs
+++ Phase-11/1.cjs
@@ -1,6 +1,8 @@
 
 
-// const axios=require ("axios");
+const axios=require ("axios");
+
+// import axios from "axios";
 
 // function fetchData(url){
 //     axios.get(url)
@@ -13,19 +15,30 @@
 // }
 // fetchData("https://jsonplaceholder.typicode.com/posts")
 
-const axios=require ('axios');
+// const axios=require ('axios');
 
-function fetchData(url){
-    axios.get(url)
-    .then((result)=>{
-        console.log(result.data.filter((task)=>task.completed==true))
-        // let tasks=result.data.filter((task)=>task.completed==true)
-        // console.log(tasks);
-    }).catch((error)=>{
-        console.error(error)
-    })
+// function fetchData(url){
+//     axios.get(url)
+//     .then((result)=>{
+//         console.log(result.data.filter((task)=>task.completed==true))
+//         // let tasks=result.data.filter((task)=>task.completed==true)
+//         // console.log(tasks);
+//     }).catch((error)=>{
+//         console.error(error)
+//     })
+// }
+// fetchData("https://jsonplaceholder.typicode.com/todos");
+
+async function fetchData(url){
+   
+    try{
+        let result= await axios.get(url)
+        console.log(result.data);
+    }catch(error){
+        console.error(error.message);
+    }
 }
-fetchData("https://jsonplaceholder.typicode.com/todos");
+fetchData("https://jsonplaceholder.typicode.com/posts");
 
 
 
